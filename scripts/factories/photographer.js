a9b762a21d9c736345de2d0376c1de3f146e7bf9
nelumbo <remi.chambiet@laposte.net>
2022-01-11 17:59:35 +0100

    fonction javascript index implementee raf mis en page css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,35 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name,id,city,country, tagline,price,portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `Sample Photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const url=new URL(location.origin + '/photographer.html');
+        const search_params=url.searchParams;
+        search_params.set('id',id)
+        const a=document.createElement('a');
+        a.setAttribute('href',url);
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h3=document.createElement( 'h3' );
+        h3.className="local";
+        h3.textContent = city+","+country;
+        const p=document.createElement('p');
+        p.className="tagline";
+        p.textContent=tagline;
+        const p1=document.createElement('p');
+        p1.className="price";
+        p1.textContent=price+"€/jour";
+        article.appendChild(a),
+        a.appendChild(img);
+        a.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(p1);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,10 @@
     async function getPhotographers() {
+            const res =await fetch('./data/photographers.json');
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+            const photographers = await res.json();
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return (
+            photographers);
     }
 
     async function displayData(photographers) {
